cbe540712d3ec892e16532d78ad0f61a552fe1bc
nikita.dotsenko <indahazzard@gmail.com>
2021-01-08 12:26:40 +0200

    test

--- xhprof/xhprof_lib/defaults.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('XHPROF_LIB_ROOT')) {
-  define('XHPROF_LIB_ROOT', dirname(__FILE__));
-}
-
-if (!defined('XHPROF_CONFIG')) {
-  define('XHPROF_CONFIG', XHPROF_LIB_ROOT . '/config.php');
-}
